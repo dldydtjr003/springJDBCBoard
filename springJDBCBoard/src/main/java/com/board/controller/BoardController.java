0d48c8b9e0f1f885daab7aace767a258957be875
dldydtjr003 <dldydtjr003@naver.com>
2026-01-27 14:45:17 +0900

    [생성] 게시판 테스트 생성

---
+++ springJDBCBoard/src/main/java/com/board/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Controller
+@Slf4j
+@RequestMapping("/board")
+public class BoardController {
+	
+	@GetMapping("/insertForm")
+	public String insertForm(Model model) {
+		
+		return "board/insertForm";
+	}
+	
+	
+}
