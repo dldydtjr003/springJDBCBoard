691ba9875ed2fa5c24e8a023cdea2420d55d15e3
dldydtjr003 <dldydtjr002@gmail.com>
2026-01-29 10:29:05 +0900

    [추가] 게시판에 커밋,롤백 기능 추가하기

--- springJDBCBoard/src/main/java/com/board/service/BoardServiceImpl.java
+++ springJDBCBoard/src/main/java/com/board/service/BoardServiceImpl.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.board.DAO.BoardDAO;
 import com.board.domain.Board;
 
 @Service
+
 public class BoardServiceImpl implements BoardService {
 
 	//3. implements를 준다
@@ -17,6 +19,7 @@ public class BoardServiceImpl implements BoardService {
 	private BoardDAO boardDAO;
 	
 	@Override
+	@Transactional
 	public int insertBoard(Board board) throws Exception {
 		int Count = boardDAO.insertBoard(board);
 		return Count;
@@ -29,12 +32,14 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	@Override
+	@Transactional
 	public int updateBoard(Board board) throws Exception {
 		int Count = boardDAO.updateBoard(board);
 		return Count;
 	}
 
 	@Override
+	@Transactional
 	public int deleteBoard(Board board) throws Exception {
 		int Count = boardDAO.deleteBoard(board);
 		return Count;
